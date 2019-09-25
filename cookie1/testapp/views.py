a34fc2edd77576fb074ee26d01f2be867e3dd985
durga <narendrapasu010@gmail.com>
2019-09-25 16:37:19 +0530

    last commit

---
+++ cookie1/testapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def name_view(request):
+    return render(request,'name.html')
+def age_view(request):
+    name = request.GET['name']
+    response= render(request,'age.html')
+    response.set_cookie('name',name)
+    return response
+def gf_view(request):
+    age = request.GET['age']
+    response = render(request,'gf.html')
+    response.set_cookie('age',age)
+    return response
+
+def result_view(request):
+    gfname = request.GET['gfname']
+    name = request.COOKIES.get('name')
+    age = request.COOKIES.get('age')
+    response= render(request,'cout.html',{'name':name,'age':age,'gfname':gfname})
+    return response
